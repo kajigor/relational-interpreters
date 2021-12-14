34da13fbaaa4891f687680233f4948967e2a04bc
Ekaterina Verbitskaia <kajigor@gmail.com>
2021-12-14 21:34:10 +0300

    Protosort

--- topsort/TopsortFun.hs
+++ topsort/TopsortFun.hs
@@ -6,6 +6,8 @@ import Control.Monad (when)
 
 import Debug.Trace ( trace )
 import Text.Printf ( printf )
+import qualified Data.List
+import Data.Maybe ( isNothing, fromJust )
 
 data Nat = O | S Nat deriving (Eq, Show)
 
@@ -17,6 +19,7 @@ instance Enum Nat where
 
 less :: Nat -> Nat -> [Bool]
 less x y =
+  -- trace ("less\n" ++ show x ++ "\n" ++ show y ++ "\n") $
   case y of
     S y' ->
       case x of
@@ -65,29 +68,97 @@ lookup limit key =
       return (x, [(O, x)])
     S k -> do
       x <- someNums limit
-      (v, xs) <- lookup limit k
-      return (v, (S k, v) : xs)
+      (_, xs) <- lookup limit k
+      return (x, (S k, x) : xs)
 
+mergeable :: Numbering -> Numbering -> Bool
+mergeable xs ys =
+    all (check ys) xs && all (check xs) ys
+  where
+    check xs (k, v) =
+      let v' = Data.List.lookup k xs
+      in isNothing v' || fromJust v' == v
+
+merge :: Numbering -> Numbering -> [Numbering]
+merge xs ys | mergeable xs ys =
+    go xs ys
+  where
+    go [] ys = [ys]
+    go (x : xs) ys | x `elem` ys = map (x :) $ go xs (x `Data.List.delete` ys)
+                   | otherwise = map (x :) $ go xs ys
+merge _ _ = []
 
 eval :: Graph -> [Numbering]
 eval g = do
-    n <- numberOfNodes g
-    go g n
+    n <- S <$> numberOfNodes g
+    go g n []
   where
-    go g n =
+    go g n nums =
       case g of
-        [] -> []
+        [] -> return nums
         ((b, e) : graph') -> do
           (nb, numbering) <- lookup n b
           (ne, numbering') <- lookup n e
-          if trace (printf "Num:  %s\nNum': %s\n" (show numbering) (show numbering')) $ numbering == numbering'
-          then do
-            True <- less nb ne
-            True <- less ne n
-            numbering'' <- go graph' n
-            if numbering == numbering''
-            then return numbering
-            else return []
-          else return []
+          numbering <- merge nums numbering
+          numbering <- merge numbering numbering'
+          True <- less nb ne
+          True <- less ne n
+          numbering'' <- go graph' n numbering
+          merge numbering numbering''
+
+unf = do
+  let nums = []
+  let n = S (S (S O))
+  let b = O
+  let e = S O
+  (nb, numbering) <- lookup n b
+  numbering <- merge nums numbering
+  (ne, numbering') <- lookup n e
+  numbering <- merge numbering numbering'
+  True <- less nb ne
+  True <- less ne n
+
+  let nums = numbering
+  let b = O
+  let e = S (S O)
+  (nb, numbering) <- lookup n b
+  numbering <- merge nums numbering
+  (ne, numbering') <- lookup n e
+  numbering <- merge numbering numbering'
+  True <- less nb ne
+  True <- less ne n
+
+  let nums = numbering
+  let b = S O
+  let e = S (S O)
+  (nb, numbering) <- lookup n b
+  numbering <- merge nums numbering
+  (ne, numbering') <- lookup n e
+  numbering <- merge numbering numbering'
+  True <- less nb ne
+  True <- less ne n
+
+  return numbering
+
+-- eval :: Graph -> [Numbering]
+-- eval g = do
+--     n <- numberOfNodes g
+--     go g n
+--   where
+--     go g n =
+--       case g of
+--         [] -> []
+--         ((b, e) : graph') -> do
+--           (nb, numbering) <- lookup n b
+--           (ne, numbering') <- lookup n e
+--           if trace (printf "Num:  %s\nNum': %s\n" (show numbering) (show numbering')) $ numbering == numbering'
+--           then do
+--             True <- less nb ne
+--             True <- less ne n
+--             numbering'' <- go graph' n
+--             if numbering == numbering''
+--             then return numbering
+--             else return []
+--           else return []
 
 
